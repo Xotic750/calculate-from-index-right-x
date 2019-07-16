984f6896fd31e2c7c6c71e218b71bd2e7d61d4a4
Graham Fairweather <xotic750@gmail.com>
2019-07-16 21:51:48 +0200

    Terraformed
    Auto build

--- src/calculate-from-index-right-x.js
+++ src/calculate-from-index-right-x.js
@@ -1,12 +1,3 @@
-/**
- * @file Calculates a fromIndexRight of a given value for an array.
- * @version 2.2.0
- * @author Xotic750 <Xotic750@gmail.com>
- * @copyright  Xotic750
- * @license {@link <https://opensource.org/licenses/MIT> MIT}
- * @module calculate-from-index-right-x
- */
-
 import toObject from 'to-object-x';
 
 import {toLength2018 as toLength} from 'to-length-x';
@@ -25,13 +16,6 @@ const getMin = function _getMin(a, b) {
  * @param {number} fromIndex - The position in this array at which to begin. A
  *  negative value gives the index of array.length + fromIndex by asc.
  * @returns {number} The calculated fromIndex. Default is 0.
- * @example
- * var calcFromIndexRight = require('calculate-from-index-right-x');
- *
- * calcFromIndexRight([1, 2, 3], 1); // 1
- * calcFromIndexRight([1, 2, 3], Infinity); // 2
- * calcFromIndexRight([1, 2, 3], -Infinity); // -Infinity
- * calcFromIndexRight([1, 2, 3], -1); // 2
  */
 export default function calcFromIndexRight(array, fromIndex) {
   const object = toObject(array);
